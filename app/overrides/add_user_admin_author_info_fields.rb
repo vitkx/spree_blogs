5663cb81091721ebb56b3a0917b5e3f4499f4cc3
vitkx <vitoxxx1206@gmail.com>
2024-08-23 02:46:54 -0500

    Fix

--- app/overrides/add_user_admin_author_info_fields.rb
+++ app/overrides/add_user_admin_author_info_fields.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
-
-Deface::Override.new(
-  virtual_path: "spree/admin/users/_form",
-  name: "admin_author_info_fields",
-  insert_bottom: "[data-hook='admin_user_form_fields']",
-  partial: "spree/admin/users/author_info_fields",
-  disabled: false
-)
+class AddUserAdminAuthorInfoFields
+  Deface::Override.new(
+    virtual_path: "spree/admin/users/_form",
+    name: "admin_author_info_fields",
+    insert_bottom: "[data-hook='admin_user_form_fields']",
+    partial: "spree/admin/users/author_info_fields",
+    disabled: false
+  )
+end
