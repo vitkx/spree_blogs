a73056ab8ed95a588d7c02b3465432e846c618b4
vitkx <vitoxxx1206@gmail.com>
2024-08-23 02:38:29 -0500

    Fix menu

--- app/overrides/add_blog_admin_menu_tab.rb
+++ app/overrides/add_blog_admin_menu_tab.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
-
-Deface::Override.new(
-  virtual_path: "spree/admin/shared/_main_menu",
-  name: "blogs_admin_sidebar_menu",
-  insert_bottom: '[data-hook="admin_tabs"]',
-  partial: "spree/admin/shared/blogs_sidebar_menu",
-  disabled: false
-)
+class AddBlogAdminMenuTab
+  Deface::Override.new(
+    virtual_path: "spree/admin/shared/_main_menu",
+    name: "blogs_admin_sidebar_menu",
+    insert_bottom: 'nav',
+    partial: "spree/admin/shared/blogs_sidebar_menu",
+    disabled: false
+  )
+end
